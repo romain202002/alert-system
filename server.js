c271c6cd0e3a0c135716759fe088280fa05f3449
romain202002 <romainchassignat130@gmail.com>
2026-01-21 21:17:29 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ import admin from "firebase-admin";
 
 const app = express();
 
-// Firebase via variable d'environnement
+// Vérifie la variable d'environnement
+if (!process.env.FIREBASE_KEY) {
+  console.error("ERREUR : FIREBASE_KEY non définie !");
+  process.exit(1);
+}
+
+// Initialisation Firebase
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-// Servir le dossier "public"
+// Servir les fichiers statiques dans "public"
 app.use(express.static("public"));
 
 // Port dynamique pour Railway
